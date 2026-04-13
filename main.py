4e51c36f5f7eb26d398866b96dbea0479d6f7f63
aminbita162-glitch <aminbita162@icloud.com>
2026-04-13 22:19:21 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,5 +3,12 @@ from fastapi import FastAPI
 app = FastAPI()
 
 @app.get("/")
-def read_root():
+def home():
     return {"message": "Patch-Auri backend is running"}
+
+@app.get("/scan")
+def scan_patch():
+    return {
+        "status": "ok",
+        "data": "NFC patch scanned successfully"
+    }
